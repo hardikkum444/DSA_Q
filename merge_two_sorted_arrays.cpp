e44aa4d3d5b5067894fa59bd4d8fa21d45f9f383
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2023-10-08 11:11:24 +0530

    added the most optimized method to this
    //The key advantage of merging from the end to the beginning is that 
    // you can do it in-place without requiring additional memory or shifting 
    // elements. When you compare elements from both arrays and place the larger 
    // element in the last available position, you avoid overwriting elements in 
    // the first part of the array, which have already been processed. This approach
    //  ensures that you don't lose any elements from nums1 during the merging process.

--- merge_two_sorted_arrays.cpp
+++ merge_two_sorted_arrays.cpp
@@ -54,3 +54,62 @@ int main()
     cout<<endl;
 
 }
+
+
+//in case, the question consists of the two sorted elements having
+
+
+// The key advantage of merging from the end to the beginning is that 
+// you can do it in-place without requiring additional memory or shifting 
+// elements. When you compare elements from both arrays and place the larger 
+// element in the last available position, you avoid overwriting elements in 
+// the first part of the array, which have already been processed. This approach
+//  ensures that you don't lose any elements from nums1 during the merging process.
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = m - 1; // Index for the last element in nums1
+    int j = n - 1; // Index for the last element in nums2
+    int k = m + n - 1; // Index for the merged array
+
+    // Merge elements from the end to the beginning
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] >= nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    // If there are remaining elements in nums2, copy them to nums1
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+}
+
+int main() {
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0}; // Initialize with zeros for extra space
+    int m = 3;
+    vector<int> nums2 = {2, 5, 6};
+    int n = 3;
+
+    merge(nums1, m, nums2, n);
+
+    for (int i = 0; i < m + n; i++) {
+        cout << nums1[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
